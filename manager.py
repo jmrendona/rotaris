37933001273be2bfb0bb26b4013855b4170fe226
Jose Rendón <jmrendona0417@gmail.com>
2026-04-16 16:36:04 -0400

    uodated manager.py

--- manager.py
+++ manager.py
@@ -2,15 +2,20 @@ from converters.forces_strip import ForcesCSVConverter
 from converters.span_2_radius import SpanToRadiusConverter
 
 forces = ForcesCSVConverter(
-    file_path='path/to/forces/files',
-    file_pattern='Forces-Strip-*.csv',
-    dt=0.001
+    file_path='/scratch/renj3003/rotor-alone/6e-5_6000rpm/data/forces_strip',
+    file_pattern='Force-Graph-*.csv',
+    dt=0.000056
 )
 
-span_to_radius = SpanToRadiusConverter(
-    input_path='path/to/spanwise/files',
-    output_path='path/to/radius/files',
-    variable_col='Variable[Force:N]',
-    span_col='SpanwiseCoordinate[Length:m]',
-    radius_resolution=1
-)
+forces.convert('forces_strips.h5')
+
+#span_to_radius = SpanToRadiusConverter(
+#    input_path='/scratch/renj3003/edat/cases/baseline/data/yplus',
+#    output_path='/scratch/renj3003/edat/cases/baseline/data/yplus/yplus_radius.h5',
+#    variable_col='Value (Up)[Dimensionless:dimensionless]',
+#    span_col='Position (Up)[Length:m]',
+#    chord_length=0.047,
+#    radius_resolution=0.047/100
+#)
+
+#span_to_radius.convert()
